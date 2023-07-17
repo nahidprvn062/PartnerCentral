e6e802dc4419403f85f95ff127cd2c5270ea06e1
Goutamgithub97 <139456882+Goutamgithub97@users.noreply.github.com>
2023-07-17 12:38:05 +0530

    Add files via upload

---
+++ productDetail.js
@@ -0,0 +1,21 @@
+import { LightningElement, wire } from 'lwc';
+import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
+import user_id from '@salesforce/user/Id';
+import user_name from '@salesforce/schema/User.Name';
+
+// import My_Resource from '@salesforce/resourceUrl/Thar';
+export default class productDetail extends LightningElement {
+    // bgImageStyle
+
+    @wire(getRecord,{recordId:user_id, fields:[user_name]})records 
+     
+   get currentUserName(){
+    return this.records.data ? getFieldValue(this.records.data, user_name ) : '';
+   }
+
+
+//    get bgImageStyle() {
+//     return `background-image: url(${My_Resource})`;
+// }
+  
+}
